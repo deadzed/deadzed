f57f268aea842de678ec9d9997d59f66ce43d51a
Dustin Lacewell <dlacewell@gmail.com>
2014-05-18 21:53:30 -0700

    Changed gulpfile to output to public

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var source = require('vinyl-source-stream');
 gulp.task('html', function() {
 	gulp.src('src/index.html')
 	.pipe($.minifyHtml())
-	.pipe(gulp.dest('app'))
+	.pipe(gulp.dest(public))
 	.on('error', $.util.log);
 });
 
@@ -14,13 +14,13 @@ gulp.task('scripts', function() {
 	browserify('./src/scripts/zed.js')
 	.bundle()
 	.pipe(source('app.js'))
-	.pipe(gulp.dest('app'))
+	.pipe(gulp.dest(public))
 	.on('error', $.util.log);
 });
 
 gulp.task("vendor", function(){
 	$.bowerFiles()
-	.pipe(gulp.dest('app'))
+	.pipe(gulp.dest(public))
 	.on('error', $.util.log);
 });
 
@@ -28,22 +28,22 @@ gulp.task('styles', function() {
 	gulp.src('src/styles/**')
 	.pipe($.concat('app.css'))
 	.pipe($.csso())
-	.pipe(gulp.dest('app'))
+	.pipe(gulp.dest(public))
 	.on('error', $.util.log);
 });
 
 gulp.task('assets', function () {
 	gulp.src('src/assets/**/*')
-	.pipe(gulp.dest('app'))
+	.pipe(gulp.dest(public))
 	.on('error', $.util.log);
 });
 
 gulp.task('connect', $.connect.server({
-	root: ['app'], port: 8080, livereload: true
+	root: [public], port: 8080, livereload: true
 }));
 
 gulp.task('clean', function () {
-	gulp.src('app', {read: false})
+	gulp.src(public, {read: false})
 	.pipe($.clean({force: true}))
 	.on('error', $.util.log);
 });
@@ -53,7 +53,7 @@ gulp.task('watch', function() {
 	gulp.watch('src/scripts/**/*', ['scripts']);
 	gulp.watch('src/styles/**/*', ['styles']);
 	gulp.watch('src/assets/**/*', ['assets']);
-	gulp.watch('app/*', function (e) {
+	gulp.watch('public/*', function (e) {
 		gulp.src(e.path)
 		.pipe($.connect.reload());
 	});
